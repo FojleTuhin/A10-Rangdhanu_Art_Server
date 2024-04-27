11d3ee39f4e303a3f04812378ba8f3d957d5dfba
fojletuhin <fojletuhin@gmail.com>
2024-04-28 03:05:02 +0600

    delete option create

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.port || 5000;
 
@@ -38,6 +38,13 @@ async function run() {
       const newItem= req.body;
       console.log(newItem);
       const result = await itemCollection.insertOne(newItem);
+      res.send(result)
+    })
+
+    app.delete('/item/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result = await itemCollection.deleteOne(query);
       res.send(result)
     })
 
